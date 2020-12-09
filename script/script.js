ed059a07e5dad91732bbbabd8f6eaca1a2d6abb0
Relwinde <ouedgv@hotmail.fr>
2020-12-09 21:31:57 +0000

    first

---
+++ script/script.js
@@ -0,0 +1,27 @@
+/*
+const actu_menu = document.querySelector(".actu_menu");
+const actu = document.querySelector(".actu");
+
+actu_menu.style.display = "none";
+
+actu.onmouseenter = () => {
+     actu_menu.style.display = "block";
+};
+
+actu.onmouseout = () => {
+     actu_menu.style.display = "none";
+};
+
+
+const img = document.querySelector('#section_2 img');
+const slide = document.querySelector('#section_2');
+const size = img.clientWidth;
+
+console.log(size);
+
+slide.onmouseover = () => {
+    slide.style.transform = 'translateX(' + (-size) + 'px)';
+    console.log('mouse enter');
+}
+
+*/
